92e45fac084dba8895afa796cdf9da3962af5def
Mani Shanmugam <shanmugam-manikandan@zai.com>
2022-02-07 18:26:05 -0500

    Add multiple server pools

--- package_deploy.sh
+++ package_deploy.sh
@@ -2,4 +2,5 @@
 helm package helm/minio-operator
 ssh minio-m1 rm -f /home/mshanmu/helm_charts/minio-operator-*.tgz
 scp minio-operator-*.tgz minio-m1:~/helm_charts
+ssh mshanmu@minio-m1 'helm upgrade --create-namespace --install -n minio-operator minio-op ~/helm_charts/minio-operator-*.tgz'
 rm -f minio-operator-*.tgz
